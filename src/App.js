e8cc62582e933ce194e10b6d45a56aa66d679742
Sergio Cortes <sergiocortessat@gmail.com>
2021-09-30 17:47:51 +0200

    Add solution to interview challenges

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
+import { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
+  const [input, setInput] = useState("")
+  const [todoItems, setTodoItems] = useState([])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setTodoItems(prev => [...prev, input])
+  }
+
+  const handleDelete = (event,index) => {
+    event.preventDefault()
+    // const newTodoList = todoItems.filter((tempItem, idx) =>( idx !== index ))
+    const newTodoList = todoItems.map((item, idx) => {
+      // if(idx !== index) {
+      //   newList.push(item)
+      // }
+      // return newList
+       
+      return idx !== index ? item :null 
+    })
+    const newList =  newTodoList.filter(i => i !== null )
+    setTodoItems(newList)
+  }
+
+
+
+
+
+  console.log(todoItems);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="search" className="form" onSubmit={(e) => handleSubmit(e)}>
+        <label for="search">
+          Task
+      </label>
+      <input type="text"  value={input} onChange={(e) => setInput(e.target.value)}/>
+      <input type="submit" />
+    </form>
+    {todoItems.map((item, index) => (
+      <div key={index}>
+        {item}
+        <button type="button" onClick={(e) => handleDelete(e,index)} >
+          Delete
+          </button>
+      </div>
+    ))}
     </div>
   );
 }
